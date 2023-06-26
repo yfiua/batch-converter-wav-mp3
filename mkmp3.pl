d5522fd7b2b9071c3a8d817007b8d7091bb4d2c7
Jun Sun <yfiua@qq.com>
2023-06-26 11:20:01 +0200

    fix utf-8 encoding

--- mkmp3.pl
+++ mkmp3.pl
@@ -1,12 +1,14 @@
 #! /usr/bin/perl -w
 use strict;
+use utf8;
 use warnings FATAL => 'all';
 
+binmode(STDIN, ":utf8");
 binmode(STDOUT, ":utf8");
 
 while (<STDIN>) {
 
-    chomp; 
+    chomp;
 
     # Split the string
     /^\.\/wav\/(.*)\.wav$/ or next;
